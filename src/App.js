85946a73b19098e4edbad4938706fd9d11a8dc57
CoderShail09 <95957280+CoderShail09@users.noreply.github.com>
2022-05-25 07:16:07 +0530

    Add files via upload
    all files have been created and initialised

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div >
+     <h1>Mobile Operating system</h1>
+    
+     <li >Blackberry</li>
+     <li >iphone</li>
+     <li >Windows phone</li>
+     <li >Android</li>
+
+
+    </div>
+    <div >
+     <h1>Mobile Manufacturers</h1>
+    
+     <li className='square'>Samsung</li>
+     <li className='square'>HTC</li>
+     <li >Micromax</li>
+     <li className='circle'>Apple</li>
+
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
